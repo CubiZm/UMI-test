ce202ed07c27d8e7c357c01eae95aa4455022193
CubiZm <karelense@gmail.com>
2019-04-28 23:46:03 +0300

    UMI-pages

--- js/main.js
+++ js/main.js
@@ -1,15 +1,71 @@
-'use strict'
+var simpleValidation = function(){
+  
+  var validateForm = $('form.form');
 
-const menuBtn = document.querySelector('.menu-button');
-const menu = document.querySelector('.menu');
-const userForm = document.querySelector('.user-form');
+  validateForm.each(function(){
+    var validateForm = $(this);
+    var validate = {};
+    var validateThis = $(this).find('.form__validate');
+    var validatingLength = $(this).find('.form__validate').length;
+    var submitBtn = $(this).find('.form__button');
+    
+    for(var i = 1; i <= validatingLength; i++){
+      validate['input'+i] = false;
+    }
+    
+    $('.submit').blur(function(){
+      var index =  $(this).prevAll().length+1;
+      var validateThisVal = $(this).val();
+      var validateThisType = $(this).attr('type');
+      
+      if(validateThisType === "email"){
+        
+        // Email regex
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        // Condition to see if Email exists
+        if(!validateThisVal.match(re)){
+          $(this).addClass('not-valid');
+          $(this).removeClass('is-valid');
+          return validate['input'+index] = false;
+        } else{
+          $(this).addClass('is-valid');
+          $(this).removeClass('not-valid');
+          return validate['input'+index] = true;
+        }
+      } else{
+        // Makes sure input is filled out
+        if(validateThisVal == ""){
+          $(this).addClass('not-valid');
+          $(this).removeClass('is-valid');
+          return validate['input'+index] = false;
+        } else{
+          $(this).addClass('is-valid');
+          $(this).removeClass('not-valid');
+          return validate['input'+index] = true;
+        }
+      }
+    });
+    
+    
+    validateForm.submit(function(event){
+      event.preventDefault();
+      
+      var falseCtn = 0;
+      for(var i = 1; i <= validatingLength; i++){
+        if(validate['input'+i] == false){
+          falseCtn++;
+        }
+      }
 
-const onClickHandler = (evt) => {
-  menu.classList.toggle('isActiveMenu');
-  userForm.classList.toggle('isActiveUser');
+      if(falseCtn > 0){
+        $(this).unbind('submit').submit();
+        $(this).click();
+      } else{
+      }
+    });
+    
+  });
+  
+};
 
-  menuBtn.classList.toggle('menu-button--open');
-  menuBtn.classList.toggle('menu-button--close');
-}
-
-menuBtn.addEventListener('click', onClickHandler);
+simpleValidation();
